52e6de28abe65b336dace8b583567d839637bfde
hkasemir <heidi.kasemir@gmail.com>
2015-05-21 16:13:49 -0700

    added styles, transitions, and blog content from wordpress site. Also fixed some html problems

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views, feed
 
 urlpatterns = [
     url(r'^$', views.BlogIndex.as_view(), name="blog"),
+    url(r'^tag/(?P<slug>\S+)$', views.TagIndex.as_view(), name="tag_index"),
+    url(r'^feed/$', feed.LatestPosts(), name="feed"),
     url(r'^(?P<slug>\S+)$', views.BlogDetail.as_view(),
         name="entry_detail"),
-    url(r'^feed/$', feed.LatestPosts(), name="feed"),
-    url(r'^tag/(?P<slug>\S+)$', views.TagIndex.as_view(), name="tag_index"),
 ]
