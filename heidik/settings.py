4095ef8c4c1076992781b717614e8239f31b9cfd
hkasemir <heidi.kasemir@gmail.com>
2015-06-20 22:56:57 -0400

    updated allowed hosts

--- heidik/settings.py
+++ heidik/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+'.heidik.name',
+]
 
 
 # Application definition
